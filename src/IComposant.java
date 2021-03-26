07b7f604daa9122efb013e4fb9ca8c961ca6936b
sambp <2032325@etu.cegepjonquiere.ca>
2021-03-25 22:05:14 -0400

    labo 7 et 8

--- src/IComposant.java
+++ src/IComposant.java
@@ -12,5 +12,8 @@ public interface IComposant {
 
     /* Pour faire le circuit en x
     (/ONNo/ONNo/ONNo/[/BVBo/VNNo/JNNo/MBNo/]/)
+
+    Ps Malheureusement j'ai manquer de temps pour tout faire vue les probleme que j'ai rencontrer et
+    toute les erreures que j'ai eu
      */
 }
